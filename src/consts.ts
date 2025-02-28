6a0808e81d3c0d3e97a65136b5fee21fa33dd0ae
Nilesh-Pithiya-Sanskar <nilesh@sanskartechnolab.com>
2025-02-28 17:45:59 +0530

    init commit

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Nilesh Pithiya';
-export const SITE_DESCRIPTION = 'Cerified Frappe/ERPNext Developer | Nilesh Pithiya';
+export const SITE_TITLE = 'Nilesh Pithiya - Certified Frappe/ERPNext Developer';
+export const SITE_DESCRIPTION = 'Hire the certified Frappe/ERPNext developer! Expert in ERP solutions, customization, and technical support. | Nilesh Pithiya';
