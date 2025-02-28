8dbd6ed03773b94c1c1d2ddedfea7931d7459060
Nilesh-Pithiya-Sanskar <nilesh@sanskartechnolab.com>
2025-02-28 14:43:02 +0530

    init commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://Pithiya-Nilesh.github.io',
+  	base: '/nilesh-pithiya',
 	integrations: [mdx(), sitemap()],
 });
